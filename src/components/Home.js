6fb3e80ad6767dfa33c6c66b299c090956d816dc
Piti <piti118@gmail.com>
2017-05-30 09:44:52 +0700

    add repo and cred

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,7 +4,15 @@ class Home extends Component {
   render() {
     return (
       <div className="home-text">
-        <h1>Welcome to Currency Quiz</h1>
+        <div>
+          <h1>Welcome to Currency Quiz</h1>
+          <div style={{ fontSize: 'small', paddingTop: '20px' }}>
+          Find the source code <a href="https://github.com/piti118/currency-quiz">here.
+        </a></div>
+          <div style={{ fontSize: 'small' }}>
+          Currency API from <a href="http://fixer.io">fixer.io</a>
+          </div>
+        </div>
       </div>
     );
   }
